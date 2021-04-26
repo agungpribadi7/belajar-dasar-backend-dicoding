9f8df353b28eebd3fffe4f11d25da689ddd913f8
Agung <omegaudayana777@gmail.com>
2021-04-26 23:31:25 +0700

    first commit

---
+++ routes.js
@@ -0,0 +1,30 @@
+const {addBooks, showAllBooks, showSpecificBook,
+  updateSpecificBook, deleteSpecificBook} = require('./handler.js');
+
+const routes = [{
+  method: 'POST',
+  path: '/books',
+  handler: addBooks,
+},
+{
+  method: 'GET',
+  path: '/books',
+  handler: showAllBooks,
+},
+{
+  method: 'GET',
+  path: '/books/{bookId}',
+  handler: showSpecificBook,
+},
+{
+  method: 'PUT',
+  path: '/books/{bookId}',
+  handler: updateSpecificBook,
+},
+{
+  method: 'DELETE',
+  path: '/books/{bookId}',
+  handler: deleteSpecificBook,
+}];
+
+module.exports = routes;
